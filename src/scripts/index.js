7b79fa4270ca848bbb774ad932764cc10b33cd13
Veronika K <129206296+Veronika0999@users.noreply.github.com>
2024-05-10 09:28:01 +0200

    add duration to work experience

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -10,3 +10,29 @@ hamburger.addEventListener("click", () => {
     bar3.classList.toggle("animateBar3");
     mobileNav.classList.toggle("openDrawer")     
 });
+
+function calculateWorkPeriod() {
+    var startDate = new Date('2023-08-01');
+    var endDate = new Date(); 
+    
+    var diff = Math.floor((endDate - startDate) / (1000 * 60 * 60 * 24));
+    var years = Math.floor(diff / 365);
+    var months = Math.floor((diff % 365) / 30);
+    
+    var durationElement = document.getElementById('duration');
+    if (endDate.getFullYear() === startDate.getFullYear() && endDate.getMonth() === startDate.getMonth()) {
+        durationElement.textContent = "Less than a month";
+    } else if (months === 0) {
+        durationElement.textContent = years + " yr";
+    } else if (years === 0) {
+        durationElement.textContent = months + " mos";
+    } else {
+        var yearsText = (years === 1) ? "yr" : "yrs";
+        durationElement.textContent = years + " " + yearsText + " " + months + " mos";
+    }
+}
+
+
+window.onload = function() {
+    calculateWorkPeriod();
+};
